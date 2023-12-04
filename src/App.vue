3ff209e39df4a559eb5f380609072816270da9cf
grossiValentina <grossivalentina435@gmail.com>
2023-12-04 12:08:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+import { store } from "./store.js";
+
+
+export default {
+  data() {
+    return {
+      store
+    };
+  },
+    
+  
+
+  methods: {
+    
+
+    },
+
+  components :{
+      AppHeader,
+      AppMain,
+      AppFooter,
+    
+}
+}
+
+</script>
+
+<template>
+<AppHeader />
+<AppMain />
+<AppFooter />
+
+ 
+</template>
+
+<style lang="scss">
+@use "./components/style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
+
+</style>
